8061fe83f5f0d50feef76846ef953f8ec47a41dd
Kian <kiandehghani2@gmail.com>
2025-12-09 08:09:58 -0500

    Added Department Class with all the methods needed

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    /**
+     * Checks whether the department name is valid or not
+     * @param departmentName that user inputs
+     * @return whether the name is valid or not
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.format("D%02d", nextId);
+            nextId++;
+        } else {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+    }
+
+    public void setDepartmentName(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+        } else {
+            System.out.println("Error: Invalid Department Name.");
+        }
+    }
+}
